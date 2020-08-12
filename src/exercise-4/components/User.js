3e076d3cab8a68a69dd2a439645ccae298d2aa67
ClarkTheSup <lknkoqq123@163.com>
2020-08-12 18:07:37 +0800

    finish exercise-4

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router';
+import NotMatch from "./NotMatch";
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
-};
+class User extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    let userId = this.props.match.params.user;
+    let regexp = /[a-zA-Z]+/;
+    let isNotNum = regexp.test(userId);
+    if(isNotNum) {
+      return <Route component={NotMatch}/>
+    }
+    return <div>User profile page.</div>
+  }
+}
 
 export default User;
